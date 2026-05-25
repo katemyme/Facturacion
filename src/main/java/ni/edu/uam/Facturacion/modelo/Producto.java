1c0ee5c8ba8b6812747570e8a37ec521c53e64b9
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-05-25 16:50:34 -0600

    Initial commit

---
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Producto.java
@@ -0,0 +1,16 @@
+package ni.edu.uam.Facturacion.modelo;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Producto {
+
+    @Id @Column(length=9)
+    int numero;
+
+    @Column(length=50) @Required
+    String descripcion;
+
+}
+
