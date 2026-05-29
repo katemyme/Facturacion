a69d73836ecb013283c6c73929dd31e0a54b0546
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-05-29 16:08:14 -0600

    Feat: Agregando el paquete calculadores y añadiendo su clase y modificando la factura

---
+++ src/main/java/ni/edu/uam/Facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
@@ -0,0 +1,36 @@
+package ni.edu.uam.Facturacion.calculadores;
+import javax.persistence.Query;
+
+import org.openxava.calculators.*;
+import org.openxava.jpa.*;
+
+import lombok.*;
+
+public class CalculadorSiguienteNumeroParaAnyo
+        implements ICalculator {
+
+    @Getter @Setter
+    private int anyo;
+
+    @Override
+    public Object calculate() throws Exception {
+
+        Query query =
+                XPersistence.getManager()
+                        .createQuery(
+                                "select max(f.numero) from Factura f where f.anyo = :anyo"
+                        );
+
+        query.setParameter(
+                "anyo",
+                anyo
+        );
+
+        Integer ultimoNumero =
+                (Integer) query.getSingleResult();
+
+        return ultimoNumero == null
+                ? 1
+                : ultimoNumero + 1;
+    }
+}
---
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
@@ -0,0 +1,49 @@
+package ni.edu.uam.Facturacion.modelo;
+
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+import ni.edu.uam.Facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
+
+@Entity
+@Getter @Setter
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class)
+    int anyo;
+
+    @Column(length=6)
+    @DefaultValueCalculator(
+            value = CalculadorSiguienteNumeroParaAnyo.class,
+            properties = @PropertyValue(
+                    name = "anyo"
+            )
+    )
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    private Cliente cliente;
+}
